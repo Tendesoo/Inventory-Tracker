2296c5bec6e9a7907b32d51b2c6d5818afebce1a
Tendeso <tendesom@afrosoft.co.zw>
2022-07-08 11:54:03 +0200

    responsive sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent  {
 
-  constructor() { }
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  ngOnInit(): void {
+
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      }else{
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+
+      }
+    })
   }
+
 
 }
